ff7b5b34533bf6600a8ea4611b1b8f1f29339811
jay <jtegge095@gmail.com>
2018-01-20 12:21:58 -0600

    fix urls

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .feeds import LatestPostsFeed
 
 urlpatterns = [
-	url(r'^$', 'path.to.function')
+	url(r'^$', 'path.to.function'),
 	url(r'^$', views.post_list, name='post_list'),
 	#url(r'^$', views.PostListView.as_view(), name='post_list'),
 	url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'r'(?P<post>[-\w]+)/$', views.post_detail, name='post_detail'),
