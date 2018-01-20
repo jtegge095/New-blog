122ff43cd18b679defbf35ff6fb3510e5c64794a
jay <jtegge095@gmail.com>
2018-01-20 12:23:51 -0600

    fix urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,6 +23,7 @@ sitemaps = {
 }
 
 urlpatterns = [
+    url(r'^$', 'path.to.function'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')),
     url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
